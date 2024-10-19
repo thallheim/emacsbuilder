8832fd1a55242c97d944a36c9f5486ebc0035a24
thallheim <thall@thallheim.com>
2024-10-19 23:05:18 +0200

    Add separate script for building without also installing

--- 29.4-dev-0.1.2-mini/build_install.sh
+++ 29.4-dev-0.1.2-mini/build_install.sh
@@ -13,7 +13,7 @@ fi
 
 if [ -f ./src-29.4/configure ]; then
     cd ./src-29.4
-    (time make -j10) && \
-        echo "Job's a good'un - ready to install"
+    time (make -j10 && make install -j10) && \
+        echo "Job's a good'un (built and installed)"
     cd ..
 fi
