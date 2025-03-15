7f579a3913f136c448caabe25fecd18a4ab5d4d0
thallheim <thall@thallheim.com>
2025-03-15 03:21:15 +0100

    Update install script for 30.1

--- 30.1-dev-0.0.1/build_install.sh
+++ 30.1-dev-0.0.1/build_install.sh
@@ -3,17 +3,19 @@ echo "------------------------"
 echo " Building"
 echo "------------------------"
 
+VERSION="30.1"
+
 export CFLAGS=-Wno-error=implicit-function-declaration
 
 
-if [ ! -f ./src-29.4/configure ]; then
+if [ ! -f ./src-$VERSION/configure ]; then
     echo "configure.sh not found - aborting."
     exit -1
 fi
 
-if [ -f ./src-29.4/configure ]; then
-    cd ./src-29.4
-    time (make -j10 && make install -j10) && \
+if [ -f ./src-$VERSION/configure ]; then
+    cd ./src-$VERSION
+    time (make -j16 && make install -j16) && \
         echo "Job's a good'un (built and installed)"
     cd ..
 fi
